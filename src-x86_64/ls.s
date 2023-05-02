9b9aa045fd7ffd427e1e0f3e1cc11d00d57fc10d
Xea <0xea@autistici.org>
2023-05-02 01:23:57 +0000

    better

--- src-x86_64/ls.s
+++ src-x86_64/ls.s
@@ -9,78 +9,71 @@ buf:
 .globl _start
 
 _start:
-	mov (%rsp), %rdx   # get argc
-	add $16, %rsp      # offset argv pointer
+	mov (%rsp), %r13        # get argc
+	add $16, %rsp           # offset argv pointer
 	
 	.file_loop:
-		cmp $1, %rdx   # check if argc is under 2
-		je .file_end   # end program
-		pop %rdi
-		mov $0x10000, %rsi
+		cmp $1, %r13        # check if argc is under 2
+		je .file_end        # end program
+		pop %rdi            # pop argument pointer
+		mov $0x10000, %rsi  # set open mode for directory
 		
 		mov $2, %rax
-		syscall        # call open
+		syscall             # call open
 		test %rax, %rax
-		js .fail
+		js .fail            # fail check
 		
-		mov %rax, %rdi
+		mov %rax, %rdi      # store fd for folder
+		mov %rax, %r8      # store fd for folder
 	.getdirents:
-		lea buf, %rsi
+		lea buf, %rsi       # load buffer address
+		mov $buf_s, %rdx    # load buffer size to count
 		mov $217, %rax
-		mov $buf_s, %rdx
-		syscall        # call getdents
+		syscall             # call getdents
 		test %rax, %rax
-		js .fail
-		mov %rax, %r13
+		js .fail            # fail check
 		
-		lea buf, %r12
+		mov %rsi, %r12      # move buffer address to r12
 	.dirent_loop:
-		#mov (%r11), %r8 # get d_ino
-		#add $16, %r11
-		#movzx 16(%r11), %r9 # get d_reclen
-		#add $2, %r11
-		#mov 18(%r11), %r10 # get d_name
-		mov (%r12), %r8
-		movzx 16(%r12), %r9
-		lea 18(%r12), %r10
+		#mov (%r12), %r8    # get d_ino
+		movzx 16(%r12), %r9 # get d_reclen
+		lea 18(%r12), %r10  # get d_name
+		inc %r10            # correct d_name by one
 		test %r9, %r9
-		jz .dirent_end
-		add %r9, %r12
-		inc %r10
-			# strlen
-				mov %r10, %rax
-				xor %rdx, %rdx
-				jmp .count_loop
-				.inc_loop:
-					inc %rax
-					inc %rdx
-				.count_loop:
-				movb (%rax), %bl
-				test %bl, %bl
-				jnz .inc_loop
-				movb $'\n', (%rax)
-				inc %rdx
-				inc %rax
-				movb $0, (%rax)
-			# print
-			mov $1, %rdi
-			mov %r10, %rsi
-			mov $1, %rax
-			syscall
-			jmp .dirent_loop
-		.dirent_end:
-		cmp $buf_s, %r12
-		jge .getdirents
-	.file_end:
-	mov %r8, %rdi
+		jz .dirent_end      # if d_reclen is 0 end loop
+		add %r9, %r12       # add d_reclen to pointer
+	.strlen:
+		mov %r10, %rax      # use rax as string pointer
+		mov $1, %rdx        # init strlen
+		jmp .count_loop
+		.inc_loop:
+		inc %rax        # increment string pointer
+		inc %rdx        # increment strlen
+		.count_loop:
+		movb (%rax), %bl    # move current char to bl
+		test %bl, %bl       # check if bl is NULL
+		jnz .inc_loop
+		movb $'\n', (%rax)  # add line break
+	.print:
+		mov $1, %rdi        # to STDOUT
+		mov %r10, %rsi      # get d_name pointer
+		mov $1, %rax
+		syscall             # call write syscall
+		jmp .dirent_loop
+	.dirent_end:
+		cmp $buf_s, %r12    # check if eob
+		je .getdirents      # if so read more dirents
+	mov %r8, %rdi           # get fd
 	mov $3, %rax
-	syscall       # call close syscall
+	syscall                 # call close syscall
+	jmp .file_loop
+	.file_end:
 	
 	xor %rdi, %rdi
-	mov $60, %rax      # call exit syscall
-	syscall
+	mov $60, %rax
+	syscall                 # call exit syscall
 	
 	.fail:
 	mov $1, %rdi
 	mov $60, %rax
-	syscall
+	syscall                 # call exit syscall
